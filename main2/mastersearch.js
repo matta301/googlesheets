9eebbbeafa3815d2606621ab94dcc50e7f5f2039
matta301 <matt_a70@hotmail.com>
2017-08-14 17:29:12 +0100

    restructured how data is passed for comparison

--- main2/mastersearch.js
+++ main2/mastersearch.js
@@ -131,7 +131,7 @@ function locationSearch() {
 
 
 
-	
+
 
 /*
 	shopDetails() Summary =============================
@@ -142,7 +142,6 @@ function locationSearch() {
 function shopDetails() {
 
 	var resultsContainer = document.getElementById('shop-details-page-container');
-
 
 	if (resultsContainer) {
 
@@ -155,8 +154,6 @@ function shopDetails() {
 		var shopDetails		  = '';
 		var GSdata = '';
 
-
-
 		// Renders the results to a node div element 
 		var service = new google.maps.places.PlacesService(document.createElement('div'));
 
@@ -167,24 +164,14 @@ function shopDetails() {
 
 		}, function(place, status) {
 	  		if (status === google.maps.places.PlacesServiceStatus.OK) {
-	  			//console.log(place)
+
+	  			var fullDetails = [];	  			
 
 	  			// Assign the data retrieved to variables to be used below. First test whether there is data by checking the node has a property
 	  			if (place.hasOwnProperty('name')) { var shopName = place.name.toLowerCase(); }
 	  			if (place.hasOwnProperty('formatted_address')) { var formattedAddress = place.formatted_address.toLowerCase(); }
 	  			if (place.hasOwnProperty('formatted_phone_number')) { var formattedPhone = place.formatted_phone_number; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('open_now')) { var openNow = place.opening_hours.open_now; }	      			
-	  			if (place.hasOwnProperty('rating')) {  var rating = place.rating; }
-	  			if (place.hasOwnProperty('website')) { var website = place.website; }	      			
-	  			if (place.hasOwnProperty('opening_hours')) { var monday = place.opening_hours.weekday_text[0]; }else { var monday = 'Monday: -'; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var tuesday   = place.opening_hours.weekday_text[1]; }else { var monday = 'Tuesday: -'; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var wednesday = place.opening_hours.weekday_text[2]; }else { var monday = 'Wednesday: -'; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var thursday  = place.opening_hours.weekday_text[3]; }else { var monday = 'Thursday: -'; }	      			
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var friday    = place.opening_hours.weekday_text[4]; }else { var monday = 'Friday: -'; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var saturday  = place.opening_hours.weekday_text[5]; }else { var monday = 'Saturday: -'; }
-	  			if (place.hasOwnProperty('opening_hours') && place.opening_hours.hasOwnProperty('weekday_text')) { var sunday    = place.opening_hours.weekday_text[6]; }else { var monday = 'Sunday: -'; }
-				if (place.hasOwnProperty('url')) { var mapUrl = place.url; }
-
+	  			
 				// lat and Lng for map center and position of marker
 				if (place.geometry.location) { var latitude  = place.geometry.location.lat(); }
 				if (place.geometry.location) { var longitude = place.geometry.location.lng(); }
@@ -202,67 +189,13 @@ function shopDetails() {
 					map: map
 				});
 
-				// Data that is returned
-/*				console.log("GM Longitude: " + place.geometry.location.lng());
-				console.log("GM Latitude: "  + place.geometry.location.lat());
-	  			console.log("GM Shop: " 	  + shopName);
-	  			console.log("GM Address: "   + formattedAddress);
-	  			console.log("GM Phone: " 	  + formattedPhone);
-	  			console.log("GM open Now: "  + openNow);
-	  			console.log("GM Rating: " 	  + rating);
-	  			console.log("GM Website: "   + website);
-	  			console.log("GM Map URL: "   + mapUrl);
-	  			console.log("GM Monday: " 	  + monday);
-	  			console.log("GM tuesday: "   + tuesday);
-	  			console.log("GM wednesday: " + wednesday);
-	  			console.log("GM thursday: "  + thursday);
-	  			console.log("GM friday: " 	  + friday);
-	  			console.log("GM saturday: "  + saturday);
-	  			console.log("GM sunday: " 	  + sunday);*/
-
 	  			// Meta Data
 	  			document.title = shopName + ' | ' + formattedAddress + ' | ' + formattedPhone;
+  				
+	  			var fullDetails = place;
 
-
-  			
-	  			googleSheetsData(shopName, formattedAddress) 
-
-
-				/*				
-				// Opening Times
-				openingTimeOutput 	+=	'<div>' +
-											'<h4>Opening Times</h4>' +
-											'<ul>' +
-												'<li>' + monday +'</li>' +
-												'<li>' + tuesday +'</li>' +
-												'<li>' + wednesday +'</li>' +
-												'<li>' + thursday +'</li>' +
-												'<li>' + friday +'</li>' +
-												'<li>' + saturday +'</li>' +
-												'<li>' + sunday +'</li>' +
-											'</ul>' +
-										'</div>';
-
-				// Main shop details
-				shopDetails 		+= 	'<div>' +
-											'<h4>' + shopName + '</h4>' +
-										'</div>' +
-										'<div class="shop-address">' +
-											'<p>' + formattedAddress + '</p>' +
-										'</div>';
-				
-				// Appends strings to html
-				if (openingTimeOutput) {
-					document.getElementById('opening-times').innerHTML = openingTimeOutput;
-				};
-
-				if (shopDetails) {
-					document.getElementById('shop-details').innerHTML = shopDetails;
-				};
-				*/
-
-				
-				//shopRating(rating);
+  				// Passes array to googleSheetsData() function where it will be compared to find a matching row in google sheets
+	  			googleSheetsData(fullDetails)
 	  		}
 		});
 	};
@@ -273,13 +206,14 @@ function shopDetails() {
 
 
 
+/*
+	shopDetails() Summary =============================
 
+	Retrieves Google sheets data as json and returns object to compareresults() function.  
+	The shop results returned from maps api is also passed into shopDetails() as an array.
 
-
-
-
-
-function googleSheetsData(shopName, formattedAddress) {	
+*/
+function googleSheetsData(fullDetails) {
 
 	// URL of data stored in google sheets.  Contains additional infomration such as servicing prices, socila media channels
 	var url	= "https://spreadsheets.google.com/feeds/list/1DgePfAxco-xVy2VdrfxkyPs3W1R5ZL5B-7zKgVf8DzI/od6/public/values?alt=json";
@@ -288,7 +222,7 @@ function googleSheetsData(shopName, formattedAddress) {
  		url: url,
  		dataType: 'json',
  		success: function (GSdata) { 		
-			compareResults(GSdata, shopName, formattedAddress); 		
+			compareResults(GSdata, fullDetails); 		
  		}
  	});
 }
@@ -296,39 +230,124 @@ function googleSheetsData(shopName, formattedAddress) {
 
 
 
-function compareResults(GSdata, shopName, formattedAddress) {
 
-	if (GSdata && shopName && formattedAddress) {
+
+/*
+	compareResults() Summary =============================
+
+	
+
+*/
+function compareResults(GSdata, fullDetails) {
 		
+	var googleMapsResults   = fullDetails;
+	var googleSheetsResults = GSdata.feed.entry;
 
-		var gsResults = GSdata.feed.entry;
+	// Checks to see if the results in google sheets are set
+	if (googleMapsResults && googleSheetsResults) {
 
-		
-		for (var i = 0; i < gsResults.length; i++) {
+		for (var i = 0; i < googleSheetsResults.length; i++) {			
 
-			var shopNameGS			  = gsResults[i].gsx$shopname.$t.toLowerCase();
-			var shopDescriptionGS 	  = gsResults[i].gsx$description.$t;
-			var shopCountyGS 		  = gsResults[i].gsx$county.$t;
-			var shopPostCodeGS 	      = gsResults[i].gsx$postcode.$t.toLowerCase();
-			var shopBasicServiceGS    = gsResults[i].gsx$basicservice.$t;
-			var shopStandardServiceGS = gsResults[i].gsx$standardservice.$t;
-			var shopFullServiceGS     = gsResults[i].gsx$fullservice.$t;
-			var shopFacebookGS     	  = gsResults[i].gsx$facebook.$t;
-			var shopTwitterGS     	  = gsResults[i].gsx$twitter.$t;
+			var GMshop = googleMapsResults.name.toLowerCase();
+			var GSshop = googleSheetsResults[i].gsx$shopname.$t.toLowerCase();
+
+			// Bicycle Shop Post code from both sets of data
+			var GMsaddress 	   = googleMapsResults.formatted_address.toLowerCase();
+			var GSshopPostCode = googleSheetsResults[i].gsx$postcode.$t.toLowerCase();
+
 			
-			
-			if (shopNameGS == shopName && formattedAddress.includes(shopPostCodeGS)) {
-				console.log("============= We have a match =============")
-				console.log("GS Name: " + shopName);
-				console.log("GS Description: " + shopDescriptionGS);
-				console.log("GS County: " + shopCountyGS);
-				console.log("GS Post Code: " + shopPostCodeGS);
-				console.log("GS Basic Service: " + shopBasicServiceGS);
-				console.log("GS Standard Service: " + shopBasicServiceGS);
-				console.log("GS Full Service: " + shopFullServiceGS);
-				console.log("GS Facebook: " + shopFacebookGS);
-				console.log("GS Twitter: " + shopTwitterGS);
-			};
-		};		
-	}	
+			// A comparison is made between both sets of data to find matching results using the shop name and the postcode
+			if(GMshop == GSshop && GMsaddress.indexOf(GSshopPostCode)) {
+				
+				// Merges both sets of results
+				var finalShopDetails = Object.assign(googleMapsResults, googleSheetsResults[i]);
+				
+				appendBicycleShopDetails(finalShopDetails);
+			}		
+		};	
+	}
 }
+
+
+/*
+	appendBicycleShopDetails() Summary =============================
+
+	
+
+*/
+function appendBicycleShopDetails(finalShopDetails) {
+	console.log(finalShopDetails);
+
+	// Bicycle shop Inofrmation 
+	var name 		= finalShopDetails.name;
+	var description	= finalShopDetails.gsx$description.$t;
+	var address 	= finalShopDetails.formatted_address;
+	var phone 		= finalShopDetails.formatted_phone_number;
+	var website		= finalShopDetails.website;
+	var rating		= finalShopDetails.rating;
+	var open 		= finalShopDetails.opening_hours.open_now;
+
+	console.log(name)
+	console.log(description)
+	console.log(address)
+	console.log(phone)
+	console.log(website)
+	console.log(rating)
+	console.log(open)
+
+	// Service prices
+	var basicService 	= finalShopDetails.gsx$basicservice.$t;
+	var standardService = finalShopDetails.gsx$standardservice.$t;
+	var fullService 	= finalShopDetails.gsx$fullservice.$t;
+
+	console.log(basicService)
+	console.log(standardService)
+	console.log(fullService)
+
+	// Social 
+	var facebook = finalShopDetails.gsx$facebook.$t;
+	var twitter	 = finalShopDetails.gsx$twitter.$t;
+	
+	console.log("Facebook - " + facebook)
+	console.log("Twitter - " + twitter)
+
+	console.log("----------------------------------------")
+	console.log("Reviews")
+	console.log("----------------------------------------")
+	for (var i = 0; i < finalShopDetails.reviews.length; i++) {
+		
+		console.log("Author - " + finalShopDetails.reviews[i].author_name)
+		console.log("Rating - " + finalShopDetails.reviews[i].rating)
+		console.log("Text - " + finalShopDetails.reviews[i].text)
+	};
+
+
+
+	var shopPage = document.getElementById('shop-details-page-container');	
+
+	if (shopPage && finalShopDetails) {
+
+
+
+
+
+
+
+		// Elements that will have info appended too
+		var openInfo = document.getElementById('opening-times');
+		var shopInfo = document.getElementById('shop-info');
+		
+
+		var shopDetailsOutput = '';
+		var opening 	  	  = '';	
+		
+
+		
+
+
+
+
+	};
+}
+
+
--- main2/shop-information.php
+++ main2/shop-information.php
@@ -16,26 +16,28 @@
 
 
 	/*	Side Bar		*/
-	#opening-times { margin-top: 0; margin-bottom: 0; padding: 30px 25px; background: #202020; color: #fff; }
+	#opening-times { margin-top: 0; margin-bottom: 0; padding: 30px 25px; /* background: #202020; color: #fff;  */}
 	#opening-times ul { padding: 0; }
 	#opening-times ul li { list-style: none; margin: 5px; }
 	
 	#main-shop-body { margin-top: 0; margin-bottom: 0; }
 	#main-shop-body .mdl-grid { padding: 0; }
 	#main-shop-body .mdl-grid .mdl-cell { margin-top: 0; margin-bottom: 0; }
-	
+	#main-shop-body .title { float: left; }
+	#main-shop-body .address { float: right; margin: 33px 0 16px; }
+	#main-shop-body .address span { text-transform: uppercase; }
+	#main-shop-body .introduction { clear: both; }
+
+
+
+
 	#rating .material-icons { color: #FFD966;}
 
-	#shop-details { position: relative; padding-top: 30px; }
+	#shop-info { position: relative; padding-top: 30px; }
+	#shop-info h4 { text-transform: capitalize; }
 	.shop-address { position: absolute; bottom: 0; right: 0; }
 
-	.rating-integer {
-		display: inline;
-		font-size: 20px;
-		position: relative;
-		top: -3px;
-		left: 15px;
-	}
+
 
 	#map-container { float: left; width: 100%; padding: 0; }
 	#map { margin: 0; }
@@ -51,13 +53,13 @@
 
 			<!-- Sidebar -->	
 			<sidebar id="opening-times" class="mdl-cell mdl-cell--3-col mdl-cell--8-col-tablet mdl-cell--4-col-phone" style="border: 1px solid green;">
-				<!-- Opening Times appended from shopDetails() -->
+				<!-- Opening Times appended from appendBicycleShopDetails() -->
 			</sidebar>
 
 			<div id="main-shop-body" class="mdl-cell mdl-cell--9-col" style=" ">
 
 				<div class="mdl-grid">
-					<section id="shop-details" class="mdl-cell mdl-cell--12-col" style=" ">
+					<section id="shop-info" class="mdl-cell mdl-cell--12-col" style=" ">
 
 					</section>
 
@@ -68,7 +70,7 @@
 					</section>
 
 					<section class="mdl-cell mdl-cell--12-col" style="border: 1px solid firebrick;">
-						<h4>Reviews</h4>
+						<h4></h4>
 						<div id="rating" class="">
 						
 						</div>
